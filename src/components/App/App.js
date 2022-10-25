d7d5e5c43a2aca687539bbc4c204efb39290c4d3
SPoluliakh <S_Poluliakh@ukr.net>
2022-10-25 17:02:23 +0300

    added coments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -27,17 +27,20 @@ export const App = () => {
     bad: 0,
   });
 
+  // Responsible for updating the state
   const leaveFeedbackByClick = evt => {
     const selectedButton = evt.currentTarget.innerText;
     dispatcher({ type: selectedButton, payload: 1 });
   };
 
+  // Responsible for counting feedbacks
   const countTotalFeedback = () => {
     const { good, bad, neutral } = state;
     const totalFeedback = good + bad + neutral;
     return totalFeedback;
   };
 
+  // Responsible for counting positive feedback
   const countPositiveFeedbackPercentage = () => {
     const { good } = state;
     const posotiveFeedback = Math.ceil((good / countTotalFeedback()) * 100);
@@ -72,60 +75,3 @@ export const App = () => {
     </Box>
   );
 };
-
-// export const App = () => {
-//   const [good, setGood] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [bad, setBad] = useState(0);
-
-//   const leaveFeedbackByClick = evt => {
-//     const selectedButton = evt.currentTarget.innerText;
-//     switch (selectedButton) {
-//       case 'good':
-//         setGood(prevState => prevState + 1);
-//         break;
-//       case 'neutral':
-//         setNeutral(prevState => prevState + 1);
-//         break;
-//       case 'bad':
-//         setBad(prevState => prevState + 1);
-//         break;
-//       default:
-//         return;
-//     }
-//   };
-
-//   const countTotalFeedback = () => {
-//     const totalFeedback = good + bad + neutral;
-//     return totalFeedback;
-//   };
-
-//   const countPositiveFeedbackPercentage = () => {
-//     const posotiveFeedback = Math.ceil((good / countTotalFeedback()) * 100);
-//     return posotiveFeedback || 0;
-//   };
-
-//   const feedbackVariants = ['good', 'neutral', 'bad'];
-//   return (
-//     <Box display="flex">
-//       <Section title="Plese leave feedback">
-//         <FeedbackOptions
-//           options={feedbackVariants}
-//           onKlick={leaveFeedbackByClick}
-//         />
-//         <StatTitle>Statistics</StatTitle>
-//         {countTotalFeedback() === 0 ? (
-//           <StatText>"There is no feedback"</StatText>
-//         ) : (
-//           <Statistics
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={countTotalFeedback}
-//             positivePercentage={countPositiveFeedbackPercentage()}
-//           />
-//         )}
-//       </Section>
-//     </Box>
-//   );
-// };
